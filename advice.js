d0d102d8eeceb3ec15c773c6736a3b74b632f1dd
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-30 17:46:06 +0100

    make it better

---
+++ advice.js
@@ -0,0 +1,34 @@
+// API
+const API_URL = "https://api.adviceslip.com/advice";
+
+function get(url) {
+    return fetch(url).then(resp => resp.json())
+}
+
+API = { get }
+
+const fontType = ["Roboto Mono", "Roboto Slab", "Abril Fatface", "Notable", "Bungee"]
+const colours = ["#FFCDD2", "#FCE4EC", "#F3E5F5", "#8C9EFF", "#90CAF9", "#80D8FF", "#80DEEA", "#B2DFDB", "#69F0AE", "#AED581", "#AED581", "#FFC400", "#BCAAA4", "#90A4AE"]
+const quoteP = document.querySelector("h2#quote")
+const bground = document.querySelector("body")
+
+function getQuotes() {
+    API.get(API_URL).then(data => addQuote(data['slip']['advice']))
+}
+
+// FUNCTIONS
+
+function addQuote(quote) {
+    quoteP.innerText = quote;
+    let fontsNum = Math.floor(Math.random()*fontType.length);
+    let coloursNum = Math.floor(Math.random()*colours.length);
+    document.getElementById("quote").style.fontFamily=fontType[fontsNum];
+    bground.style.backgroundColor = colours[coloursNum]
+}
+
+const reloadButton = document.querySelector("button#reload")
+reloadButton.addEventListener("click", ()=> getQuotes())
+
+// START PAGE
+document.body.onload = getQuotes
+
